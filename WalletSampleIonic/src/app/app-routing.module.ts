d5296e698080b5f5406840427bf76e9704d327c3
tatae <tachapong999.gmail.com>
2018-10-20 13:41:52 +0700

    login profile page

--- WalletSampleIonic/src/app/app-routing.module.ts
+++ WalletSampleIonic/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
