2c5a95ac7a9ddfc6d03eff72ad09ff4610e7553d
tatae <tachapong999.gmail.com>
2018-10-20 15:19:00 +0700

    create buy

--- WalletSampleIonic/src/app/app-routing.module.ts
+++ WalletSampleIonic/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   // { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: '', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'buy', loadChildren: './buy/buy.module#BuyPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
